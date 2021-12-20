2cc2c158ca9e233406193f01da5342e3a2f5221e
Gustavo de Camargo da Silva Ramalho <gstvramalho@gmail.com>
2021-12-20 11:32:40 -0300

    Adicionado service e logica que realiza a refatoracao e adicionado exemplo de refatoracao no main exercicio 3

---
+++ src/com/gustavo/application/exercicio2/Main.java
@@ -0,0 +1,21 @@
+package com.gustavo.application.exercicio2;
+
+import com.gustavo.models.Eleitor;
+import com.gustavo.models.Votos;
+import com.gustavo.service.ContagemVotosImpl;
+import com.gustavo.service.ContagemVotosInter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+	// write your code here
+    }
+
+}
